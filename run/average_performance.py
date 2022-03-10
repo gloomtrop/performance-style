d017c67dc01457f4ee3ab943554776cfa85e0295
Bjorn Are Therkelsen <ba.therk@outlook.com>
2022-03-10 09:44:43 +0900

    Extract notes and compute average performance

---
+++ run/average_performance.py
@@ -0,0 +1,26 @@
+import os
+from utils import paths
+import pandas as pd
+
+
+piece = 'D960'
+path = os.path.join(paths.get_root_folder(), 'processed data', piece, 'notes')
+
+file_names = paths.get_files(path)
+
+all_notes = pd.DataFrame()
+for performance_name in file_names:
+    performance_path = os.path.join(path, performance_name)
+    df = pd.read_json(performance_path)
+    all_notes = pd.concat([all_notes, df])
+
+averages = all_notes.groupby(by=all_notes.index).mean()
+stds = all_notes.groupby(by=all_notes.index).std()
+
+
+columns = ['time_onset', 'time_offset', 'velocity_onset', 'velocity_offset','duration']
+data = averages[columns].join(stds[columns], lsuffix='_avg', rsuffix='_std')
+
+file_path = os.path.join(path, 'avg.json')
+
+data.to_json(file_path)

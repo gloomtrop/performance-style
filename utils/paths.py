7e89bf3864235260ce321c54c72a4b9bd95e78c0
Bjorn Are Therkelsen <ba.therk@outlook.com>
2022-03-10 09:43:16 +0900

    Add get file names in directory

--- utils/paths.py
+++ utils/paths.py
@@ -10,3 +10,5 @@ def get_current_directory_path():
 def get_root_folder():
     return os.path.join(Path(__file__).parent.parent, '')
 
+def get_files(path):
+    return [f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]

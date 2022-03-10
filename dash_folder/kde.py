5b068547e0adfbb055781f5b535b2fe85f4fb77e
Bjorn Are Therkelsen <ba.therk@outlook.com>
2022-03-10 20:16:20 +0900

    Add dash to visualize baseline model

---
+++ dash_folder/kde.py
@@ -0,0 +1,173 @@
+import dash
+from dash import dcc
+from dash import html
+from dash.dependencies import Input, Output, State
+from dash_folder.template import assets_folder, colors
+import plotly.figure_factory as ff
+from sklearn.neighbors import KernelDensity
+from utils import paths
+import os
+import pandas as pd
+import numpy as np
+import scipy
+
+app = dash.Dash(__name__, assets_folder=assets_folder, title='KDE')
+RUN_PORT = 8059
+
+PERFORMERS = [f'p{i}' for i in range(11)]
+COLUMNS = ['time_onset', 'time_offset', 'velocity_onset', 'velocity_offset', 'duration']
+STD_COLUMNS = [name + '_standardized' for name in COLUMNS]
+TEST_PERFORMER = 0
+TEST_AMOUNT = 100
+GENERATE_POINTS = True
+GENERATED_AMOUNT = 200
+
+
+def load_data(piece='D960', split=0.8, filter='std'):
+    training_data = pd.DataFrame()
+    test_data = pd.DataFrame()
+    deviations_path = os.path.join(paths.get_root_folder(), 'processed data', piece, 'deviations')
+    deviations_names = paths.get_files(deviations_path)
+    for deviation in deviations_names:
+        data_path = os.path.join(deviations_path, deviation)
+        performer = deviation.split('-')[0]
+        data = pd.read_json(data_path)
+        length = int(data.shape[0] * split)
+        data['performer'] = performer
+        training_data = pd.concat([training_data, data[:length]])
+        test_data = pd.concat([test_data, data[length:]])
+
+    # Filtering
+    if filter == 'std':
+        columns = STD_COLUMNS + ['performer']
+    elif filter == 'unstd':
+        columns = STD_COLUMNS + ['performer']
+    else:
+        columns = COLUMNS + STD_COLUMNS + ['performer']
+
+    return training_data[columns], test_data[columns]
+
+
+def compute_entropies(sample_distributions, performer_distributions):
+    return np.array(
+        [sum([scipy.stats.entropy(sample_distributions[i], pdist[i]) for i in range(len(sample_distributions))]) for
+         pdist in performer_distributions])
+
+
+def get_sample_distributions(df, bandwidth, min_value, max_value, n_samples):
+    sample_distributions = []
+    Xs = []
+    for column in df.columns:
+        if column != 'performer':
+            X = df[column].dropna().to_numpy()
+            Xs.append(X)
+            X = X.reshape(-1, 1)
+            kde = KernelDensity(kernel='gaussian', bandwidth=bandwidth).fit(X)
+            x = np.linspace(min_value[column], max_value[column], n_samples)
+            log = kde.score_samples(x.reshape(-1, 1))
+            sample_distributions.append(np.exp(log))
+    return np.array(sample_distributions), np.array(Xs)
+
+
+def get_performer_distributions(df, bandwidth, min_value, max_value, n_samples):
+    ds = []
+    Xs = []
+    for performer in PERFORMERS:
+        mask = df['performer'] == performer
+        data = df[mask]
+        performer_ds = []
+        performer_Xs = []
+        for column in data.columns:
+            if column != 'performer':
+                X_list = data[column].dropna().to_numpy()
+                X = X_list.reshape(-1, 1)
+                kde = KernelDensity(kernel='gaussian', bandwidth=bandwidth).fit(X)
+                x = np.linspace(min_value[column], max_value[column], n_samples)
+                log = kde.score_samples(x.reshape(-1, 1))
+                performer_ds.append(np.exp(log))
+                if GENERATE_POINTS:
+                    performer_Xs.append(kde.sample(GENERATED_AMOUNT).flatten())
+                else:
+                    performer_Xs.append(X_list)
+        ds.append(np.array(performer_ds))
+        Xs.append(performer_Xs)
+    return np.array(ds), Xs
+
+
+def classify_performer(entropies):
+    index = np.argmin(np.array(entropies))
+    return PERFORMERS[index]
+
+
+bandwidth = 0.1
+n_samples = 100
+
+train, test = load_data()
+
+options = [
+    {'value': 'time_onset_standardized', 'label': 'Onset Time'},
+    {'value': 'time_offset_standardized', 'label': 'Offset Time'},
+    {'value': 'velocity_onset_standardized', 'label': 'Onset Velocity'},
+    {'value': 'velocity_offset_standardized', 'label': 'Offset Velocity'},
+    {'value': 'duration_standardized', 'label': 'Duration'}
+]
+
+min_value = train.min()
+max_value = train.max()
+
+pdist, pX = get_performer_distributions(train, bandwidth, min_value, max_value, n_samples)
+
+sample = test[test['performer'] == PERFORMERS[TEST_PERFORMER]][:TEST_AMOUNT]
+
+sdist, sX = get_sample_distributions(sample, bandwidth, min_value, max_value, n_samples)
+
+entropies = compute_entropies(sdist, pdist)
+
+colors = ['#333F44', '#37AA9C', '#94F3E4', '#33AA99', '#99FFEE', '#333F44', '#37AA9C', '#94F3E4', '#33AA99', '#99FFEE',
+          '#333F44', '#1df22f']
+colors[TEST_PERFORMER] = '#13941e'
+pX_transposed = list(map(list, zip(*pX)))
+
+
+
+app.layout = html.Div([
+    dcc.Graph(
+        id='kde',
+        style=dict(
+            height='90vh',
+            width='80vw'
+        )),
+    html.Div([
+        dcc.Dropdown(
+            id='dimension-dd',
+            clearable=False,
+            value=options[0]['value'],
+            options=options,
+            style={
+                'width': '20vw'
+            }
+        )
+    ])
+], style={
+    'display': 'flex',
+    'flex-direction': 'column',
+    'justify-content': 'center',
+    'align-items': 'center',
+    'height': '100vh',
+    'width': '100vw',
+})
+
+@app.callback(Output('kde', 'figure'),
+              Input('dimension-dd', 'value'),)
+def change_piece_options(dimension):
+    id = list(train.columns).index(dimension)
+    histograms = pX_transposed[id] + [sX[id]]
+    names = PERFORMERS + ['Sample']
+
+    # Create distplot with curve_type set to 'normal'
+    fig = ff.create_distplot(histograms, names, show_hist=False, colors=colors)
+    return fig
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True, port=RUN_PORT)

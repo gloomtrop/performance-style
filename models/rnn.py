27d13ba2e0860f46d3695e0b1cdf08acc21f0c75
Bjorn Are Therkelsen <ba.therk@outlook.com>
2022-03-22 12:45:26 +0900

    Add GRU and LSTM

--- models/rnn.py
+++ models/rnn.py
@@ -18,3 +18,40 @@ class RNN(nn.Module):
         out = out[:, -1, :]
 
         return self.fc(out)
+
+
+class GRU(nn.Module):
+
+    def __init__(self, input_size, hidden_size, num_layers, num_classes):
+        super(GRU, self).__init__()
+        self.num_layers = num_layers
+        self.hidden_size = hidden_size
+        self.gru = nn.GRU(input_size, hidden_size, num_layers, batch_first=True)
+        self.fc = nn.Linear(hidden_size, num_classes)
+
+    def forward(self, x):
+        h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size)
+
+        out, _ = self.gru(x, h0)
+        out = out[:, -1, :]
+
+        return self.fc(out)
+
+
+class LSTM(nn.Module):
+
+    def __init__(self, input_size, hidden_size, num_layers, num_classes):
+        super(LSTM, self).__init__()
+        self.num_layers = num_layers
+        self.hidden_size = hidden_size
+        self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
+        self.fc = nn.Linear(hidden_size, num_classes)
+
+    def forward(self, x):
+        h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size)
+        c0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size)
+
+        out, _ = self.lstm(x, (h0, c0))
+        out = out[:, -1, :]
+
+        return self.fc(out)
--- run/rnn.py
+++ run/rnn.py
@@ -1,7 +1,7 @@
 import numpy as np
 import torch
 
-from models.rnn import RNN
+from models.rnn import RNN, GRU, LSTM
 from utils.loading import load_split
 from utils.preprocessing import chunker
 
@@ -13,11 +13,14 @@ SEQUENCE_LENGTH = 50
 SEQUENCE_OFFSET = 10
 
 rnn = RNN(INPUT_SIZE, HIDDEN_SIZE, NUM_LAYERS, NUM_CLASSES)
+gru = GRU(INPUT_SIZE, HIDDEN_SIZE, NUM_LAYERS, NUM_CLASSES)
+lstm = LSTM(INPUT_SIZE, HIDDEN_SIZE, NUM_LAYERS, NUM_CLASSES)
 
 train, test = load_split()
 for chunk in list(chunker(train, SEQUENCE_LENGTH, SEQUENCE_OFFSET))[:1]:
     X = chunk.drop(columns=['performer']).to_numpy()
     batch = torch.tensor(np.expand_dims(X, axis=0)).float()
     print(batch.shape)
-    y_pred = rnn(batch)
-    print(y_pred)
+    print(rnn(batch))
+    print(gru(batch))
+    print(lstm(batch))

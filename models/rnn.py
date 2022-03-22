8a7bf2de555fb9b65625dd5147238a3b8b4a397e
Bjorn Are Therkelsen <ba.therk@outlook.com>
2022-03-22 12:30:24 +0900

    Add RNN

---
+++ models/rnn.py
@@ -0,0 +1,20 @@
+import torch
+import torch.nn as nn
+
+
+class RNN(nn.Module):
+
+    def __init__(self, input_size, hidden_size, num_layers, num_classes):
+        super(RNN, self).__init__()
+        self.num_layers = num_layers
+        self.hidden_size = hidden_size
+        self.rnn = nn.RNN(input_size, hidden_size, num_layers, batch_first=True)
+        self.fc = nn.Linear(hidden_size, num_classes)
+
+    def forward(self, x):
+        h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size)
+
+        out, _ = self.rnn(x, h0)
+        out = out[:, -1, :]
+
+        return self.fc(out)
---
+++ run/rnn.py
@@ -0,0 +1,23 @@
+import numpy as np
+import torch
+
+from models.rnn import RNN
+from utils.loading import load_split
+from utils.preprocessing import chunker
+
+INPUT_SIZE = 7
+HIDDEN_SIZE = 50
+NUM_LAYERS = 2
+NUM_CLASSES = 11
+SEQUENCE_LENGTH = 50
+SEQUENCE_OFFSET = 10
+
+rnn = RNN(INPUT_SIZE, HIDDEN_SIZE, NUM_LAYERS, NUM_CLASSES)
+
+train, test = load_split()
+for chunk in list(chunker(train, SEQUENCE_LENGTH, SEQUENCE_OFFSET))[:1]:
+    X = chunk.drop(columns=['performer']).to_numpy()
+    batch = torch.tensor(np.expand_dims(X, axis=0)).float()
+    print(batch.shape)
+    y_pred = rnn(batch)
+    print(y_pred)

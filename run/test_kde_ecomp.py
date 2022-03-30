908c430a388570717420030b02fa0b8ed24cb7df
Bjorn Are Therkelsen <ba.therk@outlook.com>
2022-03-30 17:02:56 +0900

    Add testing of KDE

---
+++ run/test_kde_ecomp.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+from datasets.ecomp import ECompetitionDataSet
+from models.kde import KDE_classifier
+from utils.loading import performers_last_name_list
+from utils.plotting import plot_confusion_matrix
+from utils.testing import compute_accuracy
+
+# Ignore entropy true divide errors
+np.seterr(divide='ignore', invalid='ignore')
+bandwidth = 1.2607231727912682
+n_samples = 747
+weights = [0.5465273761281865, 0.9204239858359122, 0.6225109195455564, 0.6813596379610143, 0.09445053300273365,
+           0.3725065684696263, 0.4750341463754865]
+dataset = ECompetitionDataSet(chunk_training=False, output_type='df')
+performer_ids = [i for i in range(11)]
+
+y_true = []
+y_pred = []
+
+model = KDE_classifier(dataset.train, performer_ids, weights=weights, bandwidth=bandwidth, n_samples=n_samples)
+
+for X, y in zip(*dataset.test):
+    y_pred.append(model.predict(X))
+    y_true.append(y.argmax())
+
+print(f'Accuracy: {compute_accuracy(y_true, y_pred)}')
+plot_confusion_matrix(y_true, y_pred, performer_ids, performers_last_name_list())

0692ca299593d12eb36402e400f2f7731c8945c6
Bjorn Are Therkelsen <ba.therk@outlook.com>
2022-03-30 14:36:44 +0900

    Remove data leakage in dataset, add options

--- datasets/ecomp.py
+++ datasets/ecomp.py
@@ -1,13 +1,64 @@
-import torch
-from torch.utils.data import Dataset, random_split
+import math
+import random
 
+import numpy as np
+import torch
+
+from datasets.subset import DataSubSet
 from utils.loading import load_data
 from utils.preprocessing import standardize_df, normalize_df, PERFORMERS
 from utils.testing import chunker
 
 
-class ECompetitionDataSet(Dataset):
-    def __init__(self, piece='D960', deviation_type='average', processing=None, sequence_size=50, sequence_offset=10):
+def split_time_series(time_steps, sequence_size, sequence_offset, validation_share, test_share, chunk_amounts=5):
+    all_window_starts = [i for i in range(0, time_steps - sequence_size, sequence_offset)]
+    min_window = sequence_size // sequence_offset
+
+    validation_windows = math.ceil(validation_share * len(all_window_starts))
+    validation_windows_per_chunk = validation_windows // chunk_amounts
+    test_windows = math.ceil(test_share * len(all_window_starts))
+    test_windows_per_chunk = test_windows // chunk_amounts
+    windows_per_chunk = validation_windows_per_chunk + test_windows_per_chunk
+
+    side_margin = np.arange(sequence_offset, sequence_offset * min_window, sequence_offset)
+    impossible_chunk_starts = set(side_margin)
+    impossible_chunk_starts.update(all_window_starts[-1] - side_margin)
+
+    indices = []
+
+    for i in range(chunk_amounts):
+        possible_chunk_starts = set(all_window_starts).difference(impossible_chunk_starts)
+
+        chosen_start = random.sample(possible_chunk_starts, 1)[0]
+        chosen_i = all_window_starts.index(chosen_start)
+
+        new_impossible_start = (chosen_i - windows_per_chunk + 1) * sequence_offset
+        new_impossible_end = (chosen_i + windows_per_chunk) * sequence_offset
+
+        middle = np.arange(new_impossible_start, new_impossible_end, sequence_offset)
+        left_margin = new_impossible_start - side_margin - sequence_offset
+        right_margin = new_impossible_end + side_margin
+        impossible_chunk_starts.update(np.concatenate((middle, left_margin, right_margin), axis=0))
+
+        is_test_chunk_first = bool(random.getrandbits(1))
+        if is_test_chunk_first:
+            test_end = (chosen_i + test_windows_per_chunk) * sequence_offset
+            indices.append((chosen_start, test_end, 'test'))
+            indices.append((test_end, new_impossible_end, 'validation'))
+        else:
+            validation_end = (chosen_i + test_windows_per_chunk) * sequence_offset
+            indices.append((chosen_start, validation_end, 'validation'))
+            indices.append((validation_end, new_impossible_end, 'test'))
+
+    return indices
+
+
+class ECompetitionDataSet:
+    def __init__(self, validation_share=0.1, test_share=0.1, validation_test_chunks=5,
+                 chunk_training=True,
+                 output_type='tensor', piece='D960', deviation_type='average', processing=None, sequence_size=50,
+                 sequence_offset=10):
+
         data = load_data(piece, deviation_from=deviation_type)
 
         if processing == 'standardized':
@@ -15,31 +66,67 @@ class ECompetitionDataSet(Dataset):
         elif processing == 'normalized':
             data = normalize_df(data)
 
-        self.x = []
-        self.y = []
+        train_X = []
+        train_y = []
+        validation_X = []
+        validation_y = []
+        test_X = []
+        test_y = []
 
         output_length = len(PERFORMERS)
 
-        for performer in data['performer'].unique():
+        for performer in PERFORMERS:
             performer_mask = data['performer'] == performer
             performer_data = data[performer_mask].reset_index(drop=True).drop(columns=['performer'])
 
             performer_id = PERFORMERS.index(performer)
-            y_true = torch.zeros(output_length)
+            if output_type == 'tensor':
+                y_true = torch.zeros(output_length)
+            else:
+                y_true = np.zeros(output_length)
             y_true[performer_id] = 1
 
-            for chunk in chunker(performer_data, sequence_size, sequence_offset):
-                self.x.append(torch.from_numpy(chunk.to_numpy()).float())
-                self.y.append(y_true)
-        self.len = len(self.y)
+            indices = split_time_series(len(performer_data), sequence_size, sequence_offset,
+                                        validation_share,
+                                        test_share, validation_test_chunks)
 
-    def __getitem__(self, index):
-        return self.x[index], self.y[index]
+            # Sort indices after start index
+            indices.sort(key=lambda x: x[0])
+            training_starts, training_ends = [0], [len(performer_data)]
 
-    def __len__(self):
-        return self.len
+            for (start, end, data_type) in indices:
+                df = performer_data.iloc[start:end, :]
 
-    def split(self, ratio=0.9):
-        first = int(ratio * self.__len__())
-        second = self.__len__() - first
-        return random_split(self, [first, second])
+                # Getting the ids for training data, gaps between sorted test and validation indices
+                training_starts.append(end)
+                training_ends.insert(-1, start)
+
+                for chunk in chunker(df, sequence_size, sequence_offset):
+                    if output_type == 'tensor':
+                        X = torch.from_numpy(chunk.to_numpy()).float()
+                    else:
+                        X = chunk.to_numpy()
+                    if data_type == 'test':
+                        test_X.append(X)
+                        test_y.append(y_true)
+                    elif data_type == 'validation':
+                        validation_X.append(X)
+                        validation_y.append(y_true)
+
+            for (start, end) in zip(training_starts, training_ends):
+                df = performer_data.iloc[start:end, :]
+                if chunk_training:
+                    for chunk in chunker(df, sequence_size, sequence_offset):
+                        if output_type == 'tensor':
+                            X = torch.from_numpy(chunk.to_numpy()).float()
+                        else:
+                            X = chunk.to_numpy()
+                        train_X.append(X)
+                        train_y.append(y_true)
+                else:
+                    train_X.append(df)
+                    train_y.append(y_true)
+
+        self.train = DataSubSet(train_X, train_y)
+        self.validation = DataSubSet(validation_X, validation_y)
+        self.test = DataSubSet(test_X, test_y)
--- run/test_rnn.py
+++ run/test_rnn.py
@@ -6,6 +6,7 @@ from utils.loading import load_model
 from utils.loading import performers_full_name_list
 from utils.paths import path_from_root
 from utils.plotting import plot_confusion_matrix
+from utils.testing import compute_accuracy
 
 MODEL_NAME = 'LSTM_int.pkl'
 MODEL_PATH = path_from_root('models', 'saved', MODEL_NAME)
@@ -15,7 +16,7 @@ model = load_model(MODEL_PATH)
 model.eval()
 
 dataset = ECompetitionDataSet()
-test_loader = DataLoader(dataset, batch_size=BATCH_SIZE)
+test_loader = DataLoader(dataset.test, batch_size=BATCH_SIZE)
 
 y_true = []
 y_pred = []
@@ -25,5 +26,7 @@ for i, data in enumerate(test_loader):
     outputs = model(inputs)
     y_true += torch.argmax(label, dim=1)
     y_pred += torch.argmax(outputs, dim=1)
+
+print(f'Accuracy: {compute_accuracy(y_true, y_pred)}')
 
 plot_confusion_matrix(y_true, y_pred, [torch.tensor(i) for i in range(11)], performers_full_name_list())
